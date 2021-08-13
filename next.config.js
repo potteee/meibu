6c73806def86f1aa27632d672b0fc1c96822f45f
laketownmeshi <laketownmeshi@gmail.com>
2021-08-13 20:24:10 +0900

    post/[postWorkId]/index.js getOriginalDB23

--- next.config.js
+++ next.config.js
@@ -12,8 +12,9 @@ module.exports = {
     return config;
   },
   env : {
+    url : isDevelop ? 'http://localhost:3060' : `https://lifenote.ch`
+    // url : isDevelop ? 'http://localhost:3060' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     // url : isDevelop ? process.env.NEXT_PUBLIC_VERCEL_URL : process.env.NEXT_PUBLIC_VERCEL_URL
-    url : isDevelop ? 'http://localhost:3060' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     // url : isDevelop ? 'process.env.NEXT_PUBLIC_VERCEL_URL' : 'process.env.NEXT_PUBLIC_VERCEL_URL'
     // url : isDevelop ? 'http://localhost:3060' : 'NEXT_PUBLIC_VERCEL_URL'
     // url : isDevelop ? 'http://localhost:3060' : 'VERCEL_URL'
