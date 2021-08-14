05ba029bacc92d80e5d457a07761a02e06dfe490
laketownmeshi <laketownmeshi@gmail.com>
2021-08-14 09:55:28 +0900

    post/[postWorkId]/index.js getOriginalDB30

--- src/foundations/share/GlobalConstant.js
+++ src/foundations/share/GlobalConstant.js
@@ -1,8 +1,10 @@
-function define(name, value){
-  Object.defineProperty(window, name, { 
-   get: function(){return value;},
-   set: function(){throw(name+' is already defined !!');},
-  });
-}
+// function define(name, value){
+//   Object.defineProperty(window, name, { 
+//    get: function(){return value;},
+//    set: function(){throw(name+' is already defined !!');},
+//   });
+// }
 
-define('SSG_WAIT_SEC',90)
+// define('SSG_WAIT_SEC',90)
+
+const SSG_WAIT_SEC = 90;
