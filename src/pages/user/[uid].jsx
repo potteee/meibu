6a7ef28ce225c3818f581e7ebec786fe3044344f
laketownmeshi <laketownmeshi@gmail.com>
2021-11-20 21:52:44 +0900

    preview env edit13

--- src/pages/user/[uid].jsx
+++ src/pages/user/[uid].jsx
@@ -100,8 +100,11 @@ export async function getStaticProps({ params }) {
     }
   
     const fetcher = async() => { 
-      const url = `/api/firebase/user/${params.uid}`
-      // const url = `${process.env.url}/api/firebase/user/${params.uid}`
+      // const url = `/api/firebase/user/${params.uid}`
+      const url = `${process.env.url}/api/firebase/user/${params.uid}/${context.preview 
+        ? 'preview'
+        : ''
+      }`
       // const url = `https://${process.env.url}/api/firebase/user/${params.uid}`
       // const url = `${process.env.NEXT_PUBLIC_URL}/api/firebase/user/${params.uid}`
       console.log(url+"+user url")
