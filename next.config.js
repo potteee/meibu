bb384e461b109718ceee1ce9a50dd4b18cec1fdc
laketownmeshi <laketownmeshi@gmail.com>
2021-12-16 09:17:33 +0900

    next.config.js fix

--- next.config.js
+++ next.config.js
@@ -20,15 +20,15 @@ module.exports = {
     // 注意: 上記で webpack を提供しているので、それを `require` するべきではない
     // webpack の設定のカスタマイズを実行する
     // 重要: 変更された設定を返す
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
     // config.optimizeFileTracing = false;
     return config;
   },
-  webpackDevMiddleware: config => {
-    // webpack dev middleware の設定のカスタマイズを実行する
-    // 重要: 変更された設定を返す
-    return config;
-  },
+  // webpackDevMiddleware: config => {
+  //   // webpack dev middleware の設定のカスタマイズを実行する
+  //   // 重要: 変更された設定を返す
+  //   return config;
+  // },
   // optimizeFileTracing : false,
   env : {
     url : isDevelop 
